c17c06a5eca8ac06db6ae1e5b13978f11fabacef
Suwijak Pitsupan (King) <suwijak@owndays-tech.com>
2024-07-12 14:25:35 +0700

    add package

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Test')
+let student1 = {
+    id: 1,
+    name: {
+        list: [1]
+    }
+}
+
+let student2 = JSON.parse(JSON.stringify(student1))
+
+student2.id = 2
+console.log(student2)
